ffbfd01645c65a1557559567f1120c51754372e5
Sia Ming Yean <46472108+smy1@users.noreply.github.com>
2024-04-26 06:19:59 +0800

    Update web.js

--- web.js
+++ web.js
@@ -10,37 +10,38 @@ function topFunction() {
 
 /*PRODUCE CURRENT LONG COUNT
 Get current date and Baktun 13 date*/
+const date1 = new Date("2012-12-21");
+date1.setHours(0); //Default hour is at 08:00, set it to 00:00
+const date2 = new Date();
+
+//Calculate the difference between current date and Baktun 13
+let Diff_In_Time = date2.getTime() - date1.getTime();
+let Diff_In_Days = Math.floor(Diff_In_Time / (1000 * 3600 * 24));
+
+const baktun = 13;
+let katun = Math.floor(Diff_In_Days / 7200);
+let tun = Math.floor((Diff_In_Days - (katun * 7200)) / 360);
+let winal = Math.floor((Diff_In_Days - (katun * 7200) - (tun * 360)) / 20);
+let kin = Diff_In_Days % 20;
+
 function mayan() {
-	const date1 = new Date("2012-12-21");
-	date1.setHours(0); //Default hour is at 08:00, set it to 00:00
-	const date2 = new Date();
-	
-	//Calculate the difference between current date and Baktun 13
-	let Diff_In_Time = date2.getTime() - date1.getTime();
-	let Diff_In_Days = Math.floor(Diff_In_Time / (1000 * 3600 * 24));
-	
-	const baktun = 13;
-	let katun = Math.floor(Diff_In_Days / 7200);
-	let tun = Math.floor((Diff_In_Days - (katun * 7200)) / 360);
-	let winal = Math.floor((Diff_In_Days - (katun * 7200) - (tun * 360)) / 20);
-	let kin = Diff_In_Days % 20;
-	
 	document.getElementById("longcount").innerHTML = "Today's long count: " + baktun + "." + katun + "." + tun + "." + winal + "." + kin;
-	
-	//Get Cholq'ij full name and number
-	const cholqij_list = ["Ajpu'",
-		"Imox", "Iq'", "Aq'ab'al", "K'at", 
-		"Kan", "Kame", "Kej", "Q'anil",
-		"Toj", "T'zi'", "B'atz'", "E",
-		"Aj", "I'x", "Tz'ikin", "Ajmaq",
-		"No'j", "Tijax", "Kawoq"];
-	
-	let cholqij_day = cholqij_list[kin];
-	
-	//Calculate cholqij number
-	const num = 4; //This is the cholqij number of Baktun 13
-	x = Math.floor(Diff_In_Days / 13);
-	y = 13 * x;
-	cholqij_num = Diff_In_Days - y + num;
-	document.getElementById("cholqijfull").innerHTML = cholqij_num + " " + cholqij_day;
 }
+
+//Get Cholq'ij full name and number
+const cholqij_list = ["Ajpu'",
+	"Imox", "Iq'", "Aq'ab'al", "K'at", 
+	"Kan", "Kame", "Kej", "Q'anil",
+	"Toj", "T'zi'", "B'atz'", "E",
+	"Aj", "I'x", "Tz'ikin", "Ajmaq",
+	"No'j", "Tijax", "Kawoq"];
+
+let cholqij_day = cholqij_list[kin];
+
+//Calculate cholqij number
+const num = 4; //This is the cholqij number of Baktun 13
+x = Math.floor(Diff_In_Days / 13);
+y = 13 * x;
+cholqij_num = Diff_In_Days - y + num;
+document.getElementById("cholqijfull").innerHTML = cholqij_num + " " + cholqij_day;
+
