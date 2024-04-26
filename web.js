f1402ac1bb3f504145c8dce578be34dbdb09dac6
Sia Ming Yean <46472108+smy1@users.noreply.github.com>
2024-04-26 09:03:04 +0800

    Update web.js

--- web.js
+++ web.js
@@ -42,5 +42,5 @@ cholqij_num = Diff_In_Days - y + num;
 
 function mayan() {
 	document.getElementById("longcount").innerHTML = "Today's long count: " + baktun + "." + katun + "." + tun + "." + winal + "." + kin;
-	document.getElementById("cholqijfull").innerHTML = "Today's Cholq'ij: " + cholqij_num + " " + cholqij_day;
+	document.getElementById("cholqijfull").innerHTML = "Today's Cholq'ij: " + cholqij_num + " " + cholqij_day + " (K'iche')";
 }
